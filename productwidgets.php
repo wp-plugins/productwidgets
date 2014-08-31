779c9391f32086b493db5f8097d1e8ee444048fe
krautcomputing <krautcomputing@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-31 14:28:51 +0000

    initial commit
    git-svn-id: https://plugins.svn.wordpress.org/productwidgets/trunk@976096 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ productwidgets.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package   ProductWidgets
+ * @author    Kraut Computing <info@krautcomputing.com>
+ * @license   GPL-2.0
+ * @link      http://www.productwidgets.com/publishers/wordpress/
+ * @copyright 2014 kraut computing UG (haftungsbeschränkt)
+ *
+ * @wordpress-plugin
+ * Plugin Name: ProductWidgets
+ * Plugin URI:  http://www.productwidgets.com/
+ * Description: Monetize your website with intelligent affiliate widgets displaying products that perfectly fit your unique content.
+ * Text Domain: productwidgets
+ * Version:     1.0.0
+ * Author:      Kraut Computing
+ * Author URI:  http://www.krautcomputing.com
+ * License:     GPL-2.0
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC'))
+  die;
+
+define('PW_DEV', get_bloginfo('admin_email') == 'admin@pw-dev.dev');
+
+require_once(plugin_dir_path(__FILE__).'api/class-api-response.php');
+require_once(plugin_dir_path(__FILE__).'api/class-api-request.php');
+require_once(plugin_dir_path(__FILE__).'api/api-exceptions.php');
+require_once(plugin_dir_path(__FILE__).'api-productwidgets.php');
+require_once(plugin_dir_path(__FILE__).'class-productwidgets.php');
+
+add_action('plugins_loaded', array('Product_Widgets', 'get_instance'));
