4bbdea1d8f8c6d96cebc5c61c06462953c4bc909
krautcomputing <krautcomputing@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-12 16:16:39 +0000

    fix widget layout name display
    git-svn-id: https://plugins.svn.wordpress.org/productwidgets/trunk@1066162 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/partials/widget_layouts/_name.php
+++ views/partials/widget_layouts/_name.php
@@ -1,1 +1,10 @@
-<?php echo $item["name"] ?> (<?php echo $item["width"] ?>px &times; <?php echo $item["height"] ?>px)
+<?php echo $item["name"] ?>
+
+      <?php if ($item["configuration"]["show_slider"]) { ?>
+        with slider
+
+      <?php } ?>
+<br>
+<small>
+  <?php echo $item["width"] ?>px &times; <?php echo $item["height"] ?>px
+</small>
