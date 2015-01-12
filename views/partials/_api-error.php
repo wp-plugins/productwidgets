088ffa7a4506b424a6a83e81b87f3b0c870429db
krautcomputing <krautcomputing@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-12 16:37:10 +0000

    tagged version v1.1.4
    git-svn-id: https://plugins.svn.wordpress.org/productwidgets/tags/1.1.4@1066171 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/partials/_api-error.php
@@ -0,0 +1,24 @@
+<h3>
+  Hi and welcome to ProductWidgets!
+</h3>
+<p>
+  Unfortunately, there is a tiny problem we need to solve before we can get going.
+</p>
+<p>
+  This plugin needs to be able to communicate with the ProductWidgets API (https://api.productwidgets.com).
+  <br>
+  Currently this is not possible because your hosting provider seems to block outgoing communication.
+</p>
+<p>
+  <strong>
+    Please contact your hosting provider and ask him kindly to unblock outgoing communication to https://api.productwidgets.com
+    <br>
+    The error that occurred was
+    "<?php echo $api_error ?>"
+  </strong>
+</p>
+<p>
+  Please return to this screen once this problem is fixed.
+  <br>
+  Apologies for the inconvenience!
+</p>
